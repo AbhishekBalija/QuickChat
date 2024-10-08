1c62670ef29c57f6d9490d5218518f616317e5ec
AbhishekBalija <abhiarsikere0001@gmail.com>
2024-10-08 17:22:24 +0530

    modifed-logos

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -15,11 +16,19 @@ app.use(cookieParser()); // parse cookies from request headers
 dotenv.config();
 const PORT  = process.env.PORT || 4000;
 
+const __dirname = path.resolve();
+
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRooutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
     connectToMongoDB();
